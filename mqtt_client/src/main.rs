a35fd74daf2c91aa5d62660fed0b64c3fe687552
Walker Thornley <55719041+thornleywalker@users.noreply.github.com>
2020-11-22 11:52:31 -0700

    initial

---
+++ mqtt_client/src/main.rs
@@ -0,0 +1,17 @@
+struct Config{
+ host: String,
+ port: String,
+ net_id: String,
+ action: String,
+ message: String
+}
+
+struct MqttClient{
+
+
+}
+
+fn main() {
+
+    println!("Hello, world!");
+}
